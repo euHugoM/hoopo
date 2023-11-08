c290bd78635866f08aa2aa58d40f39d59a124f9c
euHugoM <96032958+euHugoM@users.noreply.github.com>
2023-11-08 13:55:07 -0500

    add voice to speech to results

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const reset = document.querySelector(".reset");
 
 let value = 0
 
+
+
 count.addEventListener('click', function (event){
+    const utterance = new SpeechSynthesisUtterance(value)
+    let voices = speechSynthesis.getVoices()
+    utterance.voice = voices[10]
     if (value <= 107){
+        if(value > 1 && value % 10 === 0){
+            speechSynthesis.speak(utterance)
+        }
         value++;
         navigator.vibrate(100);
         count.innerHTML = value;
@@ -18,4 +26,3 @@ reset.addEventListener('click', function (event){
     value = 0;
     count.innerHTML = value;
 });
-
