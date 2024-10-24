79cc55950e9bc3eea20e068797d630a1bd4d8198
euHugoM <96032958+euHugoM@users.noreply.github.com>
2024-10-24 16:21:53 -0400

    add music, music controls and edited js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const count = document.querySelector(".counter");
 const reset = document.querySelector(".reset");
+const volume = document.getElementById('volume')
 
 let value = 0
 
+const audio = new Audio('mindfulness.mp3')
 
 
 count.addEventListener('click', function (event){
     const utterance = new SpeechSynthesisUtterance(value + 1)
     let voices = speechSynthesis.getVoices()
-    utterance.voice = voices[10]
+    utterance.voice = voices[1]
     if (value <= 107){
         value++;
         if(value > 1 && value % 10 === 0){
@@ -20,9 +22,27 @@ count.addEventListener('click', function (event){
         count.innerHTML = "Ho'opo";
         navigator.vibrate([100, 200, 100, 50, 10])
     }
+
+    audio.volume = 0.5
+    audio.play()    
 });
 
 reset.addEventListener('click', function (event){
     value = 0;
     count.innerHTML = value;
+    audio.load()
 });
+
+
+
+volume.addEventListener('click', (e) => {
+    
+    if(audio.paused){
+        audio.play()
+    }else {
+        audio.pause()
+    }
+    
+    
+})
+
